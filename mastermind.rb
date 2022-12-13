54020240ee81c8c4a07964681373b4a8c7ec9dee
Yining <ynzhang23@gmail.com>
2022-12-13 23:35:51 +0800

    Add condition for user guess input

--- mastermind.rb
+++ mastermind.rb
@@ -4,28 +4,37 @@ module Colors
   AVAILABLE_COLOR = ['R', 'O', 'Y', 'G', 'B', 'P']
 end
 
-class Attempt
-  attr_reader :attempt_array
+class Codebreaker
+  include Colors
+  attr_reader :codebreaker_array, :round_number
 
   def initialize
-    @attempt_array = [nil, nil, nil, nil]
+    @codebreaker_array = [nil, nil, nil, nil]
+    @round_number = 0
   end
 
   def get_guess
     # Instructions
-    puts "Decode!!! The available colors are: "
+    @round_number += 1
+    puts "Round #{@round_number}! The available colors are: "
     puts "-------------------------------------"
-    puts "R: Red\nO: Orange\nY: Yellow\nG: Green\nB: Blue\nP: Purple"
+    puts "R: Red        O: Orange\nY: Yellow     G: Green\nB: Blue       P: Purple"
     puts "-------------------------------------"
     # Update the current guess array
-    @attempt_array = @attempt_array.each_with_index.map do |value, index|
-    puts "Enter a color for Position #{index + 1}: " 
-    value = gets.chomp
+    @codebreaker_array = @codebreaker_array.each_with_index.map do |value, index|
+      puts "Enter a color for Position #{index + 1}: "
+      input = gets.chomp
+      # If input is not part of the six colors, ask again
+      until Colors::AVAILABLE_COLOR.include? input do
+        puts "Enter a color for Position #{index + 1}: "
+        input = gets.chomp
+      end
+      value = input
     end
   end
 
-  def update_attempt
-    return " (#{@attempt_array[0]})  (#{@attempt_array[1]})  (#{@attempt_array[2]})  (#{@attempt_array[3]}) "
+  def update_codebreaker
+    return " (#{@codebreaker_array[0]})  (#{@codebreaker_array[1]})  (#{@codebreaker_array[2]})  (#{@codebreaker_array[3]}) "
   end
 end
 
@@ -46,10 +55,10 @@ end
 generated_code = Answer.new
 puts generated_code.answer
 
-# User attempts
-attempt_board = Attempt.new
-attempt_board.get_guess
-puts attempt_board.attempt_array
+# Codebreaker attempts
+codebreaker_board = Codebreaker.new
+codebreaker_board.get_guess
+puts codebreaker_board.codebreaker_array
 
 # 
-puts attempt_board.update_attempt + " || "
+puts codebreaker_board.update_codebreaker + " || "
