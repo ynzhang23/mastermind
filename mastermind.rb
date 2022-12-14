30ad6e8e643e90cca5f29b3b895f645fbbff8772
Yining <ynzhang23@gmail.com>
2022-12-14 14:58:50 +0800

    Allow player to choose either role and create custom code as codemaker

--- mastermind.rb
+++ mastermind.rb
@@ -47,6 +47,24 @@ class Codemaker
     @feedback_array = Array.new(4)
   end
 
+  def custom_code
+    puts "Create a secret code! The available colors are: "
+    puts "-------------------------------------"
+    puts "R: Red        O: Orange\nY: Yellow     G: Green\nB: Blue       P: Purple"
+    puts "-------------------------------------"
+    # Update the current guess array
+    @code = @code.each_with_index.map do |value, index|
+      puts "Enter a color for Position #{index + 1}: "
+      input = gets.chomp
+      # If input is not part of the six colors, ask again
+      until Colors::AVAILABLE_COLOR.include? input do
+        puts "Enter a color for Position #{index + 1}: "
+        input = gets.chomp
+      end
+      value = input
+    end
+  end
+
   def give_feedback(guess_array)
     # Create duplicate array for comparison to prevent original array from being altered
     temp_code_array = @code.dup
@@ -87,20 +105,35 @@ puts "
 ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝╚═╝╚═╝░░╚══╝╚═════╝░"
 puts "---------------------------------------------------------------------------------"
 
-# Generate code
-codemaker_board = Codemaker.new
-puts codemaker_board.code
+# Allow player to choose to be "codebreaker" or "codemaker"
+puts "Choose a side! (1 or 2)"
+puts "1: Codebreaker"
+puts "2: Codemaker"
+player_choice = gets.to_i
 
-# Create Codebreaker
-codebreaker_board = Codebreaker.new
+if player_choice == 1
+  # PLAYER CHOICE: CODEBREAKER
+  # Generate code
+  codemaker_board = Codemaker.new
+  puts codemaker_board.code
+  # Create Codebreaker
+  codebreaker_board = Codebreaker.new
+  # Loop until code is correct
+  until codemaker_board.feedback_array == ['*', '*', '*', '*'] do
+    # Exit the game if round has gone beyond 12
 
-# Loop until code is correct
-until codemaker_board.feedback_array == ['*', '*', '*', '*'] do
-  # Codebreaker attempts and updates his guessed code
-  codebreaker_board.get_guess
-  # Codemaker feedback according to codebreaker's guessed code
-  codemaker_board.give_feedback(codebreaker_board.codebreaker_array)
-  # Output result of the input 
-  puts codebreaker_board.update_codebreaker_board + " || " + codemaker_board.update_codemaker_board
-  # Reset feedback tray to empty
+    # Codebreaker attempts and updates his guessed code
+    codebreaker_board.get_guess
+    # Codemaker feedback according to codebreaker's guessed code
+    codemaker_board.give_feedback(codebreaker_board.codebreaker_array)
+    # Output result of the input 
+    puts codebreaker_board.update_codebreaker_board + " || " + codemaker_board.update_codemaker_board
+    # Reset feedback tray to empty
+  end
+elsif player_choice == 2
+  # PLAYER CHOICE: CODEMAKER
+  # Ask player for code
+  codemaker_board = Codemaker.new
+  codemaker_board.custom_code
+  # Computer Guess
 end
